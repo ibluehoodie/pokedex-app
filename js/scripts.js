bcdb69811a0ea87c1ad86969a9212503e683f26a
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-03-11 23:14:42 -0500

    Expanded pokemon attribute arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 let pokemonList = [
-  {name: "Bulbasaur", height: 7, types: ["grass", "poison"]},
-  {name: "Charmander", height: 6, types: "fire"},
-  {name: "Squirtle", height: 5, types: "water"},
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+  //document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})` + '<br />');
+  if (pokemonList[i].types.length > 1){
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})` + ' - That\'s a multi-talented Pokemon!' + '<br />');
+  } else {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})` + '<br />');
+  }
+};
