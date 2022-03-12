001309162d73bb2a46eb2a32c01caf69e4d3791c
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-03-11 23:15:35 -0500

    Added conditional loop for number of Pokemon attribute types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"]
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    types: ["fire"]
+  },
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: ["water"]
+  }
 ]
 
 for (let i = 0; i < pokemonList.length; i++) {
