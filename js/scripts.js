0b17c94c47cbf9e66afec0c9cd05b72e536407c2
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-03-16 15:54:19 -0400

    Replaced for loop with forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
-let pokemonList = [
+const pokemonList = [
   {
     name: "Bulbasaur",
     height: 0.7,
-    types: ["grass", "poison"]
+    type: ["grass", "poison"]
   },
   {
     name: "Charmander",
     height: 0.6,
-    types: ["fire"]
+    type: ["fire"]
   },
   {
     name: "Squirtle",
     height: 0.5,
-    types: ["water"]
+    type: ["water"]
   }
 ]
-
-for (let i = 0; i < pokemonList.length; i++) {
-  //document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})` + '<br />');
-  if (pokemonList[i].types.length > 1){
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})` + ' - That\'s a multi-talented Pokemon!' + '<br />');
-  } else {
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})` + '<br />');
+/*
+function printArrayDetails(list) {
+  for (let i = 0; i < list.length; i++) {
+    //A pokemon meets the condition if it has more than one element under the "type" key.
+    if (list[i].type.length > 1){
+      document.write('<p>' + `${list[i].name} (height: ${list[i].height})` + ' - That\'s a multi-talented Pokemon!' + '<br />' + '</p>');
+    } else {
+      document.write('<p>' + `${list[i].name} (height: ${list[i].height})` + '<br />' + '</p>');
+    }
   }
 };
+printArrayDetails(pokemonList);
+*/
+
+//external forEach function
+pokemonList.forEach(printArrayDetails);
+  function printArrayDetails(list) {
+    document.write('<p>' + `${list.name}:` + '<br />' + `height: (${list.height})` + '<br />' + `type: (${list.type})` + '</p>');
+  };
